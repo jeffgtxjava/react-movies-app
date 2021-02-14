7e63f76b46e97227892e4c5fd0a82b692677e22e
jeffgtxjava <jeffgtxjava@gmail.com>
2021-02-14 21:19:12 +0530

    Added username and password inside Login form

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -5,6 +5,29 @@ import Button from "@material-ui/core/Button";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Modal from "react-modal";
+import { Typography } from "@material-ui/core";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%,-50%)",
+  },
+};
+
+const TabContainer = function (props) {
+  return (
+    <Typography Component="div" style={{ padding: 0 }}>
+      {props.children}
+    </Typography>
+  );
+};
 
 class Header extends Component {
   constructor() {
@@ -46,11 +69,23 @@ class Header extends Component {
           isOpen={this.state.modalIsOpen}
           contentLabel="Login"
           onRequestClose={this.closeModalHandler}
+          style={customStyles}
         >
           <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
             <Tab label="Login" />
             <Tab label="Register" />
           </Tabs>
+          <TabContainer>
+            <FormControl required>
+              <InputLabel htmlFor="userName">Username</InputLabel>
+              <Input id="userName" type="text" />
+            </FormControl>
+            <br />
+            <FormControl required>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input id="password" type="password" />
+            </FormControl>
+          </TabContainer>
         </Modal>
       </div>
     );
